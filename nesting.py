2cf2563344675d108404f4aca7fea128b9041349
njgould <njgould@users.noreply.github.com>
2022-02-01 09:03:58 +1100

    add sample data set, add allowance for kerf, improve constraints

--- nesting.py
+++ nesting.py
@@ -2,38 +2,30 @@
 # Based on https://developers.google.com/optimization/bin/bin_packing
 
 from ortools.linear_solver import pywraplp
+from nesting_sample_data import sample_parts, sample_existing_offcuts, sample_new_stock
 
-def auto_nest():
+def auto_nest(parts=None, existing_offcuts=None, new_stock=None, kerf_width = None):
 
-    parts = {
-        'p_00001': 1050,
-        'p_00002': 500,
-        'p_00003': 9000,
-        'p_00004': 8500,
-        }
+    if not parts:
+        parts = sample_parts
 
+    if not existing_offcuts:
+        existing_offcuts = sample_existing_offcuts
 
-    existing_offcuts = {
-        'os_00001':450,
-        'os_00002':300,
-        'os_00003':500,
-        }
+    if not new_stock:
+        new_stock = sample_new_stock
 
+    if not kerf_width:
+        kerf_width = 2        
 
-    new_stock = {
-        'ns_00001':10000,
-        'ns_00002':10000,
-        'ns_00003':10000, 
-        'ns_00004':10000,                      
-        }
-
+    total_part_length = sum(parts[part_id] for part_id in parts)
 
     available_stock = {**existing_offcuts, **new_stock}
 
-    # We want to minimise the requirement to purchase new stock. We therefore assign a value to each piece of stock which we can use to optimise against.
-    # New stock is given a value equal to it's length, existing offcuts are valued at 0 to prioitise thier use.
-    # Solver will minimise the value of the stock used.
-    stock_values = {stock_id:0 for stock_id in existing_offcuts}
+    # We want to minimise the stock used, but we also want to preference using existing stock. We therefore assign a value to each piece of stock which we can use to optimise against.
+    # New stock is given a value equal to thier length, existing offcuts are given a value of half thier length to prioitise use.
+    # We will set the solver to minimise the value of the stock used.
+    stock_values = {stock_id:(existing_offcuts[stock_id]//2) for stock_id in existing_offcuts}
     stock_values.update(new_stock)
 
 
@@ -61,19 +53,25 @@ def auto_nest():
 
 
     # The parts nested in each piece of stock cannot exceed the stock length.
-    # @FIXME: Need to add in an allowance for kerf
+    # We need 1 less kerf_width than the number of parts.
     for stock_id in available_stock:
-        solver.Add(sum(nest_options[(part_id, stock_id)] * parts[part_id] for part_id in parts) <= stock_options[stock_id] * available_stock[stock_id])
+        solver.Add(sum(nest_options[(part_id, stock_id)] * (parts[part_id]+kerf_width) for part_id in parts) - kerf_width <= stock_options[stock_id] * available_stock[stock_id])
 
 
     # Objective: minimize the value of the stock used
-    # @FIXME: Need to look at further optimising to maximise the consolidation of offcuts.   i.e. prefer longer offcuts to shorter.  This may be simple to do by solving again with a new objective, but constrained by the existing solution?
+    # @FIXME: Need to determine whether further optimising is required to maximise the consolidation of offcuts.   
+    # i.e. given a variety of solutions with equal minimisation of the stock value, we want to prefer the solution that has the most offcut length consilidated together so they can be used for future cutting requirements
     solver.Minimize(solver.Sum([stock_options[stock_id] * stock_values[stock_id] for stock_id in available_stock]))
+
+    # Set solver timeout in ms
+    solver.set_time_limit(5000)
 
 
     status = solver.Solve()
 
-    if status == pywraplp.Solver.OPTIMAL:
+    if status in [pywraplp.Solver.OPTIMAL, pywraplp.Solver.FEASIBLE]:
+
+        status_str = 'Optimal' if status == pywraplp.Solver.OPTIMAL else 'Feasible'
         
         stock_count = 0
         stock_length = 0
@@ -87,17 +85,32 @@ def auto_nest():
                     if nest_options[part_id, stock_id].solution_value() == 1:
                         parts_nested.append(part_id)
                         length_utilised += parts[part_id]
+                        length_remaining = available_stock[stock_id] - length_utilised
                 if parts_nested:
+                    wastage = round( (length_remaining / available_stock[stock_id]) * 100, 1 )
                     stock_count += 1
                     stock_length += available_stock[stock_id]
                     print('Stock ID: %s  %smm' %(stock_id, available_stock[stock_id]))
                     print('  Part IDs: %s' %parts_nested)
                     print('  Length Utilised: %smm' %length_utilised)
-                    print('  Total Offcut: %smm' %(available_stock[stock_id] - length_utilised))                    
-                    print()
-        print()
-        print('Total Stock lengths used: %s' %stock_count)
+                    print('  Offcut: %smm' %length_remaining) 
+                    print('  Wastage: {}%\n\n'.format(wastage))                   
+
+        total_offcuts = stock_length - total_part_length
+        total_wastage = round( (total_offcuts / stock_length) * 100, 1 )
+        print('Total pieces of stock used: %s' %stock_count)
+        print('Total stock length required: %s' %stock_length)
+        print('Total parts nested: %s' %len(parts))
+        print('Total length of parts nested: %s' %total_part_length)
+        print('Total length of offcuts: %s' %total_offcuts)
+        print('Total wastage: {}%\n\n'.format(total_wastage))  
+
+
+        print('Solver Status: %s' %status_str)
+        print('Time to solve = %s milliseconds' %solver.WallTime())
+        print('Solved in %s iterations' %solver.iterations()) 
 
 
     else:
         print('No solution....??? ')
+
